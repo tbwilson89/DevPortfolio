b360fc38dd641e6b0dcbcbeeae7ea668b79ec52a
Trevor Wilson <trevorbwilson89@gmail.com>
2019-04-06 14:45:51 -0500

    Update portfolio page style and add script to deal with animation changes.

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -7,3 +7,10 @@ $('.page-transition').on('click', function(){
   $('.active').removeClass('active')
   $(`${data.element}-link`).addClass('active')
 })
+
+$('.project-header').on('click', function() {
+  console.log('test')
+  console.log(`${$(this).data('info')}-icon`)
+  $($(this).data('info')).toggleClass('project-information-show')
+  $(`${$(this).data('info')}-icon`).toggleClass('header-icon-rotate')
+})
