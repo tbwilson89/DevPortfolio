2c58e8028bf6fdd6e44a32059cd1c7eb3f8fcf86
Trevor Wilson <trevorbwilson89@gmail.com>
2019-04-11 21:04:08 -0500

    Adding database usage for adding projects dynamically as well as being able to submit messages on contact page to the database

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,4 +1,4 @@
-
+var db = firebase.database()
 
 $('.page-transition').on('click', function(){
   let data = $(this).data('transition')
@@ -8,9 +8,44 @@ $('.page-transition').on('click', function(){
   $(`${data.element}-link`).addClass('active')
 })
 
-$('.project-header').on('click', function() {
-  console.log('test')
-  console.log(`${$(this).data('info')}-icon`)
-  $($(this).data('info')).toggleClass('project-information-show')
-  $(`${$(this).data('info')}-icon`).toggleClass('header-icon-rotate')
+db.ref('projects').once('value', (snap)=>{
+  console.log(snap.val())
+  snap.val().forEach((item)=>{
+    $('#portfolio').append(`
+      <div class="portfolio-container">
+        <div class="project-container">
+          <div id='${item.order}-project' class="project-header" data-info="#${item.order}-info">
+            <h2>${item.name}</h2><i id='${item.order}-info-icon' class="fas fa-chevron-down fa-2x"></i>
+          </div>
+          <div id='${item.order}-info' class="project-information-container">
+            <div class="project-information">
+              <img src=${item.image} alt="question block">
+              <div class="project-description">
+                <p>${item.description}</p>
+                <div class="project-links">
+                  <a href=${item.repo} target='_blank'><button class='btn btn-primary' type="button">Github Repo</button></a>
+                  <a href=${item.ghpage} target='_blank'><button class='btn btn-primary' type="button">Live Page</button></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      `)
+  })
+  $('.project-header').on('click', function() {
+    $($(this).data('info')).toggleClass('project-information-show')
+    $(`${$(this).data('info')}-icon`).toggleClass('header-icon-rotate')
+  })
 })
+
+function leaveMessage(e){
+  e.preventDefault()
+  db.ref('messages').push({
+    name: $('#input-name').val(),
+    email: $('#input-email').val(),
+    message: $('#input-message').val()
+  })
+}
+
+$('.contact-form').on('submit', leaveMessage)
